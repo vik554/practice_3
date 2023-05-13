7819602ee55afb5183ea5c085b9c85ea6d1ab78a
vik554 <kumbharvikram27@gmail.com>
2023-05-13 12:08:01 +0530

    ADDED:code added to count frquency of array

--- src/practice/Array_Program.java
+++ src/practice/Array_Program.java
@@ -4,6 +4,7 @@ public class Array_Program {
     public static void main(String[] args) {
         Array_Program s1 = new Array_Program();
         s1.printarray();
+        s1.frequencyofarray();
     }
 
 
@@ -14,5 +15,40 @@ public class Array_Program {
 
 
         }
+    }
+    public void frequencyofarray(){
+        int[] arr = { 1, 2, 3, 4, 3, 2, 1, 1, 3, 4, 5, 5, 5 };
+
+        int[] frequencyArr = findFrequency(arr);
+
+        for (int i = 0; i < frequencyArr.length; i++)
+        {
+            if (frequencyArr[i] > 0) {
+                System.out.println("Element " + (i + 1) + " occurs " + frequencyArr[i] + " times");
+            }
+        }
+    }
+
+    public static int[] findFrequency(int[] arr)
+    {
+        int[] frequencyArr = new int[arr.length];
+
+        for (int i = 0; i < arr.length; i++)
+        {
+            int count = 0;
+
+            if (frequencyArr[i] != -1)
+            {
+                for (int j = i + 1; j < arr.length; j++) {
+                    if (arr[i] == arr[j])
+                    {
+                        count++;
+                        frequencyArr[j] = -1;
+                    }
+                }
+                frequencyArr[i] = count + 1;
+            }
+        }
+        return frequencyArr;
     }
 }
