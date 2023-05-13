10618bc73c4211c7edf50fe5174bbf8639b92091
vik554 <kumbharvikram27@gmail.com>
2023-05-13 13:12:13 +0530

    ADDED:smallest element in array

--- src/practice/Array_Program.java
+++ src/practice/Array_Program.java
@@ -6,6 +6,7 @@ public class Array_Program {
         s1.printarray();
         s1.frequencyofarray();
         s1.largestelement();
+        s1.smallestelement();
     }
 
 
@@ -66,5 +67,20 @@ public class Array_Program {
             }
         }
         System.out.println("Maximum value in the array is:"+max);
+    }
+    public void smallestelement(){
+        int min = 0;
+        int a[]={44,3,6,55,43,22};
+        for(int i = 0; i < a.length; i++)
+            min = a[0];
+
+        for(int i = 0; i < a.length; i++)
+        {
+            if(min > a[i])
+            {
+                min = a[i];
+            }
+        }
+        System.out.println("Minimum value in the array is:"+min);
     }
 }
